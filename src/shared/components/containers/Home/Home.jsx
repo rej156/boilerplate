0d81f2c8c4e9bc94771327bfd100bbe39f972669
Eric John Juta <eric.juta@arcadiagroup.co.uk>
2016-05-29 23:27:19 +0100

    Added async await example of submitting messages over websockets

--- src/shared/components/containers/Home/Home.jsx
+++ src/shared/components/containers/Home/Home.jsx
@@ -8,7 +8,8 @@ export default class Home extends Component {
   }
 
   componentDidMount() {
-    this.context.store.messages.service.on('created', (message) => console.log(message))
+    this.context.store.messages.service.on('created', (message) =>
+      this.context.store.messages.addToMessages(message))
   }
 
   @action resetTimer = () => this.context.store.ticker.timer = 0
@@ -16,13 +17,19 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <p onClick={() => this.context.store.messages.createMessage('Hello world')}>Hello world</p>
+        <form onSubmit={this.context.store.messages.createMessage}>
+          <input
+            value={this.context.store.messages.message}
+            onChange={this.context.store.messages.updateMessageText} />
+          <button type="submit">Send message</button>
+        </form>
         <p onClick={this.resetTimer}>Reset Timer</p>
         <p onClick={() => this.context.store.ticker.incrementTimer(2)}>Increment timer via store setter function</p>
         <p onClick={() => this.context.store.ticker.timer += 3}>Increment by mutation ++</p>
         <p>^^^ Actually you can't do this in mobx strict mode which is enabled.</p>
         <p>Timer from observable: {this.context.store.ticker.timer}</p>
         <p>Timer from computed getter: {this.context.store.ticker.timerMultiplied}</p>
+        { this.context.store.messages.messages.map(({id, text}) => (<p key={id}>{text}</p>)) }
       </div>
     )
   }
--- src/shared/stores/messages-store.js
+++ src/shared/stores/messages-store.js
@@ -1,9 +1,10 @@
-import { observable } from 'mobx'
+import { observable, action, computed } from 'mobx'
 import client from '../lib/feathers-api-client.js'
 
 export default class MessagesStore {
 
   @observable messages = []
+  @observable message = ''
 
   constructor(messages) {
     Object.assign(this, messages);
@@ -11,9 +12,22 @@ export default class MessagesStore {
 
   get service() { return client.service('api/messages') }
 
-  createMessage(message) {
-    this.service.create({ text: message })
-      .then((message) => console.log(message)).catch(err => console.log(err))
+  @action addToMessages = (message) => {
+    this.messages.push(message)
+    this.message = ''
+  }
+
+  @action updateMessageText = (e) => this.message = e.target.value
+
+  retrieveMessageByID = (id) => console.log(this.messages.filter((message) => message.id === id))
+
+  @action createMessage = async (e) => {
+    e.preventDefault()
+    try {
+      const createdMessage = await this.service.create({ text: this.message })
+      this.addToMessages(createdMessage)
+    }
+    catch(e) { console.log(e) }
   }
 
 }
